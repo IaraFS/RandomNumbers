4638a1ce26969af705e4e4b5ddd99e7eb74a7de5
Iara Franzina Serra <iarafranzina@gmail.com>
2023-08-22 11:35:23 -0300

    Loading code

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,41 @@
-public class Main {
+import java.util.Random;
+import java.util.Scanner;
+import java.util.random.RandomGenerator;
+
+class JogoDeAdivinhacao {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner numeroDigitado = new Scanner(System.in);
+        int numeroGerado = new Random().nextInt(100);
+        int tentativas = 0;
+
+        while (tentativas < 5){
+            System.out.println("Digite um número de 0 a 100: ");
+            int numedoDigitado = numeroDigitado.nextInt();
+            tentativas ++;
+
+        if (numedoDigitado == numeroGerado){
+            System.out.println("Você acertou o número em " +tentativas + "tentativas");
+            break;   }
+
+        else if (numedoDigitado < numeroGerado) {
+            System.out.println("O número digitado é menor que o número gerado");
+        }
+        else {
+                System.out.println("O número digitado é maior que o número gerado");
+            }
+        }
+
+        if (tentativas == 5){
+            System.out.println("Você não conseguiu adivinhar o número em 5 tentativas. O número era: " + numeroGerado);
+        }
+
+
+
+
+
+
+
+
+
     }
 }
